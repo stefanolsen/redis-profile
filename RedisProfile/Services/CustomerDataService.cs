c456c8d00784979356b836b5cd7fec1c846651d0
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2017-05-25 14:10:22 +0200

    Add service methods for deleting and validating a logged in user.

--- RedisProfile/Services/CustomerDataService.cs
+++ RedisProfile/Services/CustomerDataService.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
 namespace RedisProfile.Services
 {
     public class CustomerDataService
     {
+        private const string UserTokenKey = "usertokens";
+
         public CustomerDataService()
         {
+        }
+
+        public async Task<bool> ValidateProfileExistsAsync(Guid userToken, long userId)
+        {
+            using (var connection = await GetConnection())
+            {
+                var database = connection.GetDatabase();
+
+                string tokenString = userToken.ToString("N");
+
+                RedisValue idValue = await database.HashGetAsync(UserTokenKey, tokenString, CommandFlags.PreferSlave);
+
+                return !idValue.IsNullOrEmpty &&
+                    idValue.IsInteger &&
+                    idValue == userId;
+            }
+        }
+
+        public async Task DeleteProfileDataAsync(Guid userToken, long userId)
+        {
+            using (var connection = await GetConnection())
+            {
+                var database = connection.GetDatabase();
+
+                string tokenString = userToken.ToString("N");
+
+                await database.HashDeleteAsync(UserTokenKey, tokenString, CommandFlags.DemandMaster | CommandFlags.FireAndForget);
+            }
+        }
+
+        private async static Task<ConnectionMultiplexer> GetConnection()
+        {
+            return await ConnectionMultiplexer.ConnectAsync("localhost");
         }
     }
 }
