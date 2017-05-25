94d9cb753d3446d2837448844de3e824cf0051c6
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2017-05-25 13:22:46 +0200

    Add properties to BasicData class.

--- RedisProfile/Services/BasicData.cs
+++ RedisProfile/Services/BasicData.cs
@@ -3,8 +3,18 @@ namespace RedisProfile.Services
 {
     public class BasicData
     {
-        public BasicData()
-        {
-        }
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string AddressLine1 { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public bool HasMarketingPermission { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
