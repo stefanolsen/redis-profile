a01f1f6cd262def0af17f7c80dbe6a6093fb3e1c
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2017-06-03 12:39:24 +0200

    Add more data service functionality.

--- RedisProfile/Services/CustomerDataService.cs
+++ RedisProfile/Services/CustomerDataService.cs
@@ -6,38 +6,104 @@ namespace RedisProfile.Services
 {
     public class CustomerDataService
     {
-        private const string UserTokenKey = "usertokens";
+        private const int DefaultTokenExpiryMinutes = 5;
+        private const int DefaultDataExpiryMinutes = 120;
 
-        public CustomerDataService()
-        {
-        }
+        private const string NextUserKeyKey = "next_user_key";
+        private const string UserTokenKeyFormat = "tokens:{0}";
+        private const string ProfileKeyFormat = "profiles:{0}";
 
-        public async Task<bool> ValidateProfileExistsAsync(Guid userToken, long userId)
-        {
-            using (var connection = await GetConnection())
-            {
-                var database = connection.GetDatabase();
-
-                string tokenString = userToken.ToString("N");
-
-                RedisValue idValue = await database.HashGetAsync(UserTokenKey, tokenString, CommandFlags.PreferSlave);
-
-                return !idValue.IsNullOrEmpty &&
-                    idValue.IsInteger &&
-                    idValue == userId;
-            }
-        }
-
-        public async Task DeleteProfileDataAsync(Guid userToken, long userId)
+        public async Task<bool> ValidateUserTokenExistsAsync(Guid userToken)
         {
             using (var connection = await GetConnection())
             {
                 var database = connection.GetDatabase();
 
-                string tokenString = userToken.ToString("N");
+                long? userId = await GetUserId(database, userToken, refreshTtl: true);
+                bool exists = userId.HasValue;
 
-                await database.HashDeleteAsync(UserTokenKey, tokenString, CommandFlags.DemandMaster | CommandFlags.FireAndForget);
+                return exists;
             }
+        }
+
+        public async Task DeleteUserDataAsync(Guid userToken)
+        {
+            using (var connection = await GetConnection())
+            {
+                var database = connection.GetDatabase();
+
+                long? userId = await GetUserId(database, userToken);
+                if (!userId.HasValue)
+                {
+                    return;
+                }
+
+                string tokenString = userToken.ToString("N");
+                string tokenKey = string.Format(UserTokenKeyFormat, tokenString);
+                string profileKey = string.Format(ProfileKeyFormat, userId);
+
+                await database.KeyDeleteAsync(tokenKey, CommandFlags.DemandMaster | CommandFlags.FireAndForget);
+                await database.KeyDeleteAsync(profileKey, CommandFlags.FireAndForget);
+            }
+        }
+
+        public async Task StoreUserToken(Guid userToken, long userId)
+        {
+            using (var connection = await GetConnection())
+            {
+                var database = connection.GetDatabase();
+
+                // Generate a user token key (including the Guid).
+                string tokenString = userToken.ToString("N");
+                string tokenKey = string.Format(UserTokenKeyFormat, tokenString);
+
+                // Store the user id under the user token key.
+                // Set it to expire after N minutes of not being accessed.
+                await database.StringSetAsync(
+                    tokenKey, userId, TimeSpan.FromMinutes(DefaultTokenExpiryMinutes),
+                    When.Always, CommandFlags.DemandMaster);
+            }
+        }
+
+        public async Task StoreProfileDataAsync(Guid userToken, BasicData data)
+        {
+            using (var connection = await GetConnection())
+            {
+                var database = connection.GetDatabase();
+
+                long? userId = await GetUserId(database, userToken);
+                if (!userId.HasValue)
+                {
+                    return;
+                }
+
+                string profileKey = string.Format(ProfileKeyFormat, userId);
+                var hashes = data.ToHashEntries();
+
+                await database.HashSetAsync(profileKey, hashes);
+                await database.KeyExpireAsync(profileKey, TimeSpan.FromMinutes(DefaultDataExpiryMinutes));
+            }
+        }
+
+        private async Task<long?> GetUserId(IDatabase database, Guid userToken, bool refreshTtl = false)
+        {
+            string tokenString = userToken.ToString("N");
+            string tokenKey = string.Format(UserTokenKeyFormat, tokenString);
+
+            RedisValue idValue = await database.StringGetAsync(tokenKey);
+
+            bool exists = !idValue.IsNullOrEmpty;
+            if (exists)
+            {
+                if (refreshTtl)
+                {
+                    await database.KeyExpireAsync(tokenKey, TimeSpan.FromMinutes(DefaultTokenExpiryMinutes), CommandFlags.FireAndForget);
+                }
+
+                return (long)idValue;
+            }
+
+            return null;
         }
 
         private async static Task<ConnectionMultiplexer> GetConnection()
--- RedisProfile/Services/ObjectExtensions.cs
+++ RedisProfile/Services/ObjectExtensions.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
+using System.Linq;
+using System.Reflection;
+
 namespace RedisProfile.Services
 {
-    public class ObjectExtensions
+    public static class ObjectExtensions
     {
-        public ObjectExtensions()
+        public static HashEntry[] ToHashEntries(this object obj)
         {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            var entries = properties
+                .Where(x => x.GetValue(obj) != null)
+                .Select(property => new HashEntry(property.Name, property.GetValue(obj)
+                    .ToString())).ToArray();
+
+            return entries;
+        }
+
+        public static T ConvertFromRedis<T>(this HashEntry[] hashEntries)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            var obj = Activator.CreateInstance(typeof(T));
+
+            foreach (var property in properties)
+            {
+                HashEntry entry = hashEntries.FirstOrDefault(g => g.Name.ToString().Equals(property.Name));
+                if (entry == null)
+                {
+                    continue;
+                }
+
+                property.SetValue(obj, Convert.ChangeType(entry.Value.ToString(), property.PropertyType));
+            }
+
+            return (T)obj;
         }
     }
 }
